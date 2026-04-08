3b2c11fbbefb9291761e7ee08bc9c70df9f09725
Angelo Mendes <angelo030825@gmail.com>
2026-04-08 01:37:53 -0300

    refactor: small change in `App.jsx` to suport `SmoothScroll` effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import { Header, Contact, Projects, Hero } from "./sections";
 import CustomCursor from "./components/CustomCursor.jsx";
+import SmoothScroll from "./components/smoothScroll.jsx";
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="fixed top-0 left-0 w-screen h-screen content-[''] z-10 pointer-events-none bg-[url('https://www.ui-layouts.com/noise.gif')] opacity-[0.05]"></div>
-      <Header />
-      <Hero />
-      <Projects />
-      <Contact />
+      <SmoothScroll>
+        <Header />
+        <Hero />
+        <Projects />
+        <Contact />
+      </SmoothScroll>
       <CustomCursor />
     </BrowserRouter>
   );
